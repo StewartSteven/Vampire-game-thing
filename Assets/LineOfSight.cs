e77a16615cbc181abc8ccdb11285e6cb0cd99b5c
Steven Stewart <stewart.steven98@outlook.com>
2019-03-03 21:30:18 -0500

    Initialization

---
+++ Assets/LineOfSight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    GameObject player = GameObject.Find("Player");
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && PlayerMovement. )
+        {
+            Debug.Log("I see you");
+        }
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float Speed;
+    public bool isLit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isLit = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            rb.velocity = new Vector3(-Speed, rb.velocity.y, 0f);
+           // transform.localScale = new Vector3(-1, transform.localScale.y, 1);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            rb.velocity = new Vector3(Speed, rb.velocity.y, 0f);
+           // transform.localScale = new Vector3(1, transform.localScale.y, 1);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Light"))
+        {
+            isLit = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Light"))
+        {
+            isLit = false;
+        }
+    }
+}
