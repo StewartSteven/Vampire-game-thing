14e7842157944a21567a2dac19bc0467cffb3e41
Steven Stewart <stewart.steven98@outlook.com>
2019-03-05 01:55:15 -0500

    Starting game manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+    public bool isLit = false;
+    private void Awake()
+    {
+        if (gm == null) gm = this;
+        else if (gm != null) Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setLit()
+    {
+        if (!isLit)
+        {
+           isLit = true;
+        }
+        else
+        {
+            isLit = false;
+        }
+
+    }
+}
