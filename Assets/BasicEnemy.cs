cad9ae1014e037f870d660848a2e93d3aea5f4d6
jedklika <43482414+jedklika@users.noreply.github.com>
2019-03-07 19:14:28 -0500

    Patrol AI
    Enemy patrols the platform

--- Assets/BasicEnemy.cs
+++ Assets/BasicEnemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BasicEnemy : MonoBehaviour
 {
+    public float speed;
+    public float distance;
+    private bool movingright = true;
+    public Transform groundDetection;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,20 @@ public class BasicEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if(groundInfo.collider == false)
+        {
+            if (movingright == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingright = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingright = true;
+            }
+        }
     }
 }
